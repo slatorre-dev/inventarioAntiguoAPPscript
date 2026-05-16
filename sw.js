818192610837b9ed90b63df7c20236f56bcf6694
Sebantonio <sebantonio@gmail.com>
2026-05-16 10:29:03 +0200

    nuevo sw

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // Para forzar a los clientes a coger version nueva basta con subir VERSION.
 // ═════════════════════════════════════════════════════════
 
-const VERSION = 'v80';
+const VERSION = 'v81';
 const CACHE_SHELL   = 'inventario-fp-shell-' + VERSION;
 const CACHE_RUNTIME = 'inventario-fp-runtime-' + VERSION;
 
